258eee00fa53c01ef0fa610f6df1450ffcd854a5
Ashutosh Chauhan <100701424+theashu02@users.noreply.github.com>
2024-09-14 18:39:50 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Ensure lodash/debounce is not in this array
+    },
+  },
   optimizeDeps: {
     include: ['lodash/debounce'],
   },
