6d80367d5d77825e48297980fe6c037238179489
Ashutosh Chauhan <100701424+theashu02@users.noreply.github.com>
2024-09-14 22:38:43 +0530

    fix errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,10 +26,12 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
-      }
-    }
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
