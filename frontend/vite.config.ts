fa9525597c24c97c05c0ca5b2407cf628f8ae2f5
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-09-14 18:52:11 +0530

    main deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'url';
+import path from 'path';
+
+// Resolve __dirname and __filename
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
