7d4dd9953c72de6b54837ae9f276d69199e33ef2
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-09-14 18:32:21 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const imageRouter = require('../backend/src/imageRoutes.js'); // Adjust the path as needed
+const dotenv = require("dotenv");
+dotenv.config(); 
 
 const app = express();
 const PORT = 5000;
+// const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -14,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); // Increase t
 
 // Use the image router
 app.use('/api', imageRouter);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Serve index.html for any other route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
